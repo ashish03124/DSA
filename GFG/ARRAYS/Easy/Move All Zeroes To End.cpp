35524fd941c875a2d0fe1aa306ebf0a647d9199e
Ashish Garud <ashishgarud539@gmail.com>
2025-07-14 17:40:02 +0530

    added new changes

--- GFG/ARRAYS/Easy/Move All Zeroes To End.cpp
+++ GFG/ARRAYS/Easy/Move All Zeroes To End.cpp
@@ -1,3 +1,85 @@
 //
 // Created by Agaru on 7/14/2025.
 //
+#include <bits/stdc++.h>
+using namespace std;
+//USING EXTRA SPACE O(N)
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i=0;i<n;i++) {
+//         cin >> arr[i];
+//     }
+//     vector<int> temp;
+//     for (int i=0;i<n;i++) {
+//         if (arr[i]!=0) {
+//             temp.push_back(arr[i]);
+//         }
+//     }
+//     for (int i=0;i<n;i++) {
+//         if (arr[i]==0) {
+//             temp.push_back(arr[i]);
+//         }
+//     }
+//     for (int i=0;i<n;i++) {
+//         cout << temp[i] << " ";
+//     }
+//     return 0;
+// }
+
+//TWO TRAVERSAL
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i=0;i<n;i++) {
+//         cin >> arr[i];
+//     }
+//     int count = 0;
+//     for (int i=0;i<n;i++) {
+//         if (arr[i] != 0) {
+//             arr[count++] = arr[i];
+//         }
+//     }
+//     while (count < n) {
+//         arr[count++] = 0;
+//     }
+//     for (int i=0;i<n;i++) {
+//         cout << arr[i] << " ";
+//     }
+// }
+
+//ONE TRAVERSAL
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i=0;i<n;i++) {
+//         cin >> arr[i];
+//     }
+//     int count = 0;
+//     for (int i=0;i<n;i++) {
+//         if (arr[i]!=0) {
+//             swap(arr[i], arr[count]);
+//             count++;
+//         }
+//     }
+//     for (int i=0;i<n;i++) {
+//         cout << arr[i] << " ";
+//     }
+// }
+//USING STANDARD LIBRARY
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+    stable_partition(arr.begin(), arr.end(), [](int n){ return n != 0;
+});
+    for (int x : arr) {
+        cout << x << " ";
+    }
+}
--- GFG/ARRAYS/Easy/Reverse Array in Groups.cpp
+++ GFG/ARRAYS/Easy/Reverse Array in Groups.cpp
@@ -1,3 +1,27 @@
 //
 // Created by Agaru on 7/14/2025.
 //
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+    for (int i=0;i<n;i+=k) {
+        int left = i;
+        int right = min(i+k-1, n-1);
+        while (left<right) {
+            swap(arr[left++], arr[right--]);
+        }
+    }
+    for (int i : arr) {
+        cout << i << " ";
+    }
+    return 0;
+
+}
