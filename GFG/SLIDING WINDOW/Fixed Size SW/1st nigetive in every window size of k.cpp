523bb10183a9285feaccb2d347b653bfd9f69069
Ashish Garud <ashishgarud539@gmail.com>
2025-08-12 22:06:43 +0530

    added new changes

--- GFG/SLIDING WINDOW/Fixed Size SW/1st nigetive in every window size of k.cpp
+++ GFG/SLIDING WINDOW/Fixed Size SW/1st nigetive in every window size of k.cpp
@@ -6,6 +6,17 @@ using namespace std;
 void FirstNegative(vector<int> arr, int k) {
     int i =0;
     int j =0;
+    int n = arr,size();
+    while (j<n) {
+        if (j-i+1 <k) {
+            j++;
+        }
+        else if (j-i+1 == k) {
+        if (list.size() == 0) {
+
+        }
+        }
+    }
 
 }
 int main() {
