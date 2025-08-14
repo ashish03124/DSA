997064512c99fa9d42e560fcc0b48adab3935a15
Ashish Garud <ashishgarud539@gmail.com>
2025-08-14 21:26:29 +0530

    added new changes

--- GFG/DYNAMIC PROGRAMMING/0 1 kanpsack/subest sum.cpp
+++ GFG/DYNAMIC PROGRAMMING/0 1 kanpsack/subest sum.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Agaru on 8/13/2025.
-//
+
+#include<bits/stdc++.h>
+using namespace std;
+bool subsetsum(vector<int>& arr) {
+    int sum = 0;
+    for (int n : arr) {
+        sum += n;
+    }
+    if (sum%2==1) return false;
+    int partitionsum = sum/2;
+    vector<bool> dp(partitionsum+1, false);
+    dp[0] = true;
+    for (int num : arr) {
+        for (int i = partitionsum; i >= num; i++) {
+            dp[i] = dp[i] || dp[i-num];
+        }
+    }
+    return dp[partitionsum];
+}
+int main() {
+    vector<int> arr = {1, 5, 11, 5};
+    cout << subsetsum(arr) << endl ;
+    return 0;
+}
