311fe04958ac6c71b6a0735485f93fd757996e9d
Ashish Garud <ashishgarud539@gmail.com>
2025-08-28 23:11:37 +0530

    added new changes

--- GFG/DYNAMIC PROGRAMMING/DP Problems/ClimbStairs.cpp
+++ GFG/DYNAMIC PROGRAMMING/DP Problems/ClimbStairs.cpp
@@ -1,4 +1,16 @@
-//
-// Created by Agaru on 8/26/2025.
-//
-int
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
