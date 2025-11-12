2cc6f665d98e7a00859d9c860920c005142d7df9
Ashish Garud <ashishgarud539@gmail.com>
2025-11-12 22:59:31 +0530

    added new changes

---
+++ GFG/DYNAMIC PROGRAMMING/DP Problems/dungeon game.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Agaru on 11/12/2025.
+//
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& arr) {
+        int n = arr.size();
+        int m = arr[0].size();
+        vector<vector<int>> t(n + 1, vector<int>(m + 1, INT_MAX));
+
+        t[n][m - 1] = 1;
+        t[n - 1][m] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int minHealth = min(t[i + 1][j], t[i][j + 1]) - arr[i][j];
+                t[i][j] = max(1, minHealth);
+            }
+        }
+
+        return t[0][0];
+    }
+};
---
+++ GFG/DYNAMIC PROGRAMMING/Unbounded knapsack/rod cutting.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Agaru on 11/12/2025.
+//
